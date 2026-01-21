3b37ba19a3815c14d4bfa9ea898246d93f599865
rayGnarok696 <128182328+rayGnarok696@users.noreply.github.com>
2026-01-21 17:37:41 -0600

    Fix: se removio import no utilizado de App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+//import React from 'react';
 //import { Heart, Clock, MapPin, Calendar, Users, MessageCircle } from 'lucide-react';
 import WeddingWebsite from '../src/pages/WeddingWebsite';
 
